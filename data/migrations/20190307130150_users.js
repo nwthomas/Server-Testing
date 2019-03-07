60691852012db18060c8ac2274c988d93cc93bee
Nathan Thomas <nwthomas@me.com>
2019-03-07 13:38:14 -0800

    Build out dbConfig and usersModel prior to writing tests for API routes

--- data/migrations/20190307130150_users.js
+++ data/migrations/20190307130150_users.js
@@ -1,7 +1,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable("users", tbl => {
     tbl.increments();
-    tbl.string("username", 128);
+    tbl.string("username", 128).notNullable();
     tbl.string("password", 256);
     tbl.unique("username", "uq_user_username");
   });
